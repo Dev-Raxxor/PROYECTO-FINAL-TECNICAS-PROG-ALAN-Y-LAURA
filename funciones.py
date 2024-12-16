50a82a5d62a0538e9ee8f2c386797df4a1d3ebde
Dev_Raxxor <ing.alansoto@gmail.com>
2024-12-16 00:05:42 -0500

    Otro updated más

--- funciones.py
+++ funciones.py
@@ -356,124 +356,370 @@ def PosiblesComprasPorPresupuesto (presupuesto:float, format):
     else:
         print(format + "No hay combinaciones posibles.")
 
-# 5
+#La función AnalisisClientesProductos cuenta con un algoritmo ineficiente utilizando fuerza bruta, el algoritmo eficiente lo
+
+#5
+
 def AnalisisClientesProductos():
+
     import csv;
-    #Menú para seleccionar la categoria en la que se desea conocer el cliente que mas ha comprado 
+
+    #Menú para seleccionar la categoría en la que se desea conocer el cliente que mas ha comprado 
+
     def seleccionar_categoria():
+
         def mostrar_menu():
+
             print("Seleccione una categoría:\n1. Ropa\n2. Electrónica\n3. Alimentos\n")
+
             
+
         def leer_opcion():
+
             try:
-                opcion = int(input("Elija una opción (0 para salir): "))
-                return opcion
+
+                opción = int(input("Elija una opción (0 para salir): "))
+
+                return opción
+
             except ValueError:
+
                 print("Por favor, ingrese un número válido.")
+
                 return -1
 
-        categoria_seleccionada = None  # Variable para guardar el string de la categoría
-        while True:
-            mostrar_menu()
-            opcion = leer_opcion()
 
-            if opcion == 1:
-                categoria_seleccionada = "Ropa"
+
+        categoría_seleccionada = None  # Variable para guardar el string de la categoría
+
+        while True:
+
+            mostrar_menu()
+
+            opción = leer_opcion()
+
+
+
+            if opción == 1:
+
+                categoría_seleccionada = "Ropa"
+
                 break
-            elif opcion == 2:
-                categoria_seleccionada = "Electronica"
+
+            elif opción == 2:
+
+                categoría_seleccionada = "Electronica"
+
                 break
-            elif opcion == 3:
-                categoria_seleccionada = "Alimentos"
+
+            elif opción == 3:
+
+                categoría_seleccionada = "Alimentos"
+
                 break
-            elif opcion == 0:
+
+            elif opción == 0:
+
                 print("Saliendo del programa...")
-                categoria_seleccionada = None  # No hay selección
+
+                categoría_seleccionada = None  # No hay selección
+
                 break
+
             else:
+
                 print("Opción inválida. Por favor, ingrese una opción válida.")
 
-        return categoria_seleccionada  # Retorna el string de la categoría seleccionada
 
-    # Se llama a la funcion seleccionar_categoria para que mjuestre el menú, además se le muestra al usuario que opción que selecciono
-    categoria_seleccionada = seleccionar_categoria()  # Llamada a la función
-    if categoria_seleccionada:
-        print(f"La categoría seleccionada es: {categoria_seleccionada}")
+
+        return categoría_seleccionada  # Retorna el string de la categoría seleccionada
+
+
+
+    # Se llama a la función seleccionar_categoria para que mjuestre el menú, además se le muestra al usuario que opción que seleccionó
+
+    categoría_seleccionada = seleccionar_categoria()  # Llamada a la función
+
+    if categoría_seleccionada:
+
+        print(f"La categoría seleccionada es: {categoría_seleccionada}")
+
     else:
+
         print("No se seleccionó ninguna categoría.")
+
         return  # Salir si no se seleccionó ninguna categoría
 
+
+
     # Leer CSV de ventas
+
     ventas = []
-    with open('ventas.csv', 'r') as archivo_csv:
+
+    with open('./archivosActualizadosTiendaCsv/ventasActualizadas.csv', 'r') as archivo_csv:
+
         lector_csv = csv.reader(archivo_csv)
+
         next(lector_csv)
+
         #El id clientes, id productos y cantidad se ingresaran en una lista 
+
         for fila in lector_csv:
+
             id_cliente = int(fila[2])  # El ID de cliente está en la columna 2
+
             cantidad = int(fila[3])    # La cantidad está en la columna 3
+
             id_producto = int(fila[1]) # El ID del producto está en la columna 1
+
             ventas.append([id_cliente, id_producto, cantidad])
 
+
+
     # Leer CSV de clientes y almacenar los datos en una lista
+
     clientes = []
-    with open('clientes.csv', 'r') as archivo_clientes:
+
+    with open('./archivosTiendaCsv/clientes.csv', 'r') as archivo_clientes:
+
         lector_clientes = csv.reader(archivo_clientes)
+
         next(lector_clientes)  # Omitir cabecera
+
         #El id clientes, y nombre clientes se ingresaran en otra lista 
+
         for fila in lector_clientes:
+
             id_cliente = int(fila[0])  # ID del cliente está en la columna 0
+
             nombre_cliente = fila[1]
+
             clientes.append([id_cliente, nombre_cliente])
 
+
+
     # Leer CSV de productos y almacenar los datos en una lista
+
     productos = []
-    with open('productos.csv', 'r') as archivo_productos:
+
+    with open('./archivosTiendaCsv/productos.csv', 'r') as archivo_productos:
+
         lector_productos = csv.reader(archivo_productos)
+
         next(lector_productos)  # Omitir cabecera
+
         for fila in lector_productos:
+
             id_producto = int(fila[0])  # ID del producto está en la columna 0
-            categoria = fila[2]  # Categoría del producto está en la columna 1
-            productos.append([id_producto, categoria])
+
+            categoría = fila[2]  # Categoría del producto está en la columna 1
+
+            productos.append([id_producto, categoría])
+
+
 
     # Se agrega a la lista ventas 
+
     for venta in ventas:
+
         id_cliente = venta[0]
+
         id_producto = venta[1]
 
+
+
         # Buscar el nombre del cliente 
+
         nombre_cliente = "Desconocido"
+
         for cliente in clientes:
+
             if cliente[0] == id_cliente:
+
                 nombre_cliente = cliente[1]
+
                 break
+
+
 
         # Buscar la categoría del producto 
-        categoria_producto = "Desconocida"
+
+        categoría_producto = "Desconocida"
+
         for producto in productos:
+
             if producto[0] == id_producto:
-                categoria_producto = producto[1]
+
+                categoría_producto = producto[1]
+
                 break
 
+
+
         venta.append(nombre_cliente)  # Agregar el nombre del cliente a la entrada de venta
-        venta.append(categoria_producto)  # Agregar la categoría a la entrada de venta
+
+        venta.append(categoría_producto)  # Agregar la categoría a la entrada de venta
+
+
 
     # Filtrar ventas por categoría seleccionada y encontrar la cantidad más alta en la categoría 
+
     max_cantidad = -float('inf')
+
     cliente_maximo = None
+
     for venta in ventas:
-        if venta[4] == categoria_seleccionada:  # Verificar si la categoría coincide con la seleccionada
+
+        if venta[4] == categoría_seleccionada:  # Verificar si la categoría coincide con la seleccionada
+
             if venta[2] > max_cantidad:
+
                 max_cantidad = venta[2]
+
                 cliente_maximo = venta[3]
 
-    # Mostrar el resultado
-    if cliente_maximo:
-        print(f"{cliente_maximo} tiene la mayor cantidad de compras con un total de  {max_cantidad} compras en la categoría {categoria_seleccionada}.")
-    else:
-        print(f"No se encontraron ventas en la categoría {categoria_seleccionada}")
 
-    # Imprimir la lista final de ventas (no)
-    print("\nEstado final de la lista de ventas:")
+
+    # Mostrar el resultado
+
+    if cliente_maximo:
+
+        print(f"{cliente_maximo} tiene la mayor cantidad de compras con un total de  {max_cantidad} compras en la categoría {categoría_seleccionada}.")
+
+    else:
+
+        print(f"No se encontraron ventas en la categoría {categoría_seleccionada}")
+
+
+
+#Función auxiliar para la opción 1 MostrarResumenInicial, en esta se devuelve una matriz, see utiliza la lógica de la opción 6 
+
+def AuxiliarMostrarResumenInicial():
+
+    import csv
+
+
+
+    # Leer CSV de ventas
+
+    ventas = []
+
+    with open('./archivosActualizadosTiendaCsv/ventasActualizadas.csv', 'r') as archivo_csv:
+
+        lector_csv = csv.reader(archivo_csv)
+
+        next(lector_csv)
+
+        # El id clientes, id productos y cantidad se ingresarán en una lista 
+
+        for fila in lector_csv:
+
+            id_cliente = int(fila[2])  # El ID de cliente está en la columna 2
+
+            cantidad = int(fila[3])    # La cantidad está en la columna 3
+
+            id_producto = int(fila[1]) # El ID del producto está en la columna 1
+
+            ventas.append([id_cliente, id_producto, cantidad])
+
+
+
+    # Leer CSV de clientes y almacenar los datos en una lista
+
+    clientes = []
+
+    with open('./archivosTiendaCsv/clientes.csv', 'r') as archivo_clientes:
+
+        lector_clientes = csv.reader(archivo_clientes)
+
+        next(lector_clientes)  # Omitir cabecera
+
+        # El id clientes y nombre clientes se ingresarán en otra lista 
+
+        for fila in lector_clientes:
+
+            id_cliente = int(fila[0])  # ID del cliente está en la columna 0
+
+            nombre_cliente = fila[1]
+
+            clientes.append([id_cliente, nombre_cliente])
+
+
+
+    # Leer CSV de productos y almacenar los datos en una lista
+
+    productos = []
+
+    with open('./archivosTiendaCsv/productos.csv', 'r') as archivo_productos:
+
+        lector_productos = csv.reader(archivo_productos)
+
+        next(lector_productos)  # Omitir cabecera
+
+        for fila in lector_productos:
+
+            id_producto = int(fila[0])  # ID del producto está en la columna 0
+
+            categoría = fila[2]  # Categoría del producto está en la columna 2
+
+            nombre = fila[1]  # Nombre del producto está en la columna 1
+
+            productos.append([id_producto, nombre, categoría])
+
+
+
+    # Se agrega a la lista ventas 
+
     for venta in ventas:
-        print(venta)
+
+        id_cliente = venta[0]
+
+        id_producto = venta[1]
+
+        
+
+
+
+        # Buscar el nombre del cliente 
+
+        nombre_cliente = "Desconocido"
+
+        for cliente in clientes:
+
+            if cliente[0] == id_cliente:
+
+                nombre_cliente = cliente[1]
+
+                break
+
+
+
+        # Buscar la categoría del producto y el nombre del producto 
+
+        nombre = "Desconocido"
+
+        categoría_producto = "Desconocida"
+
+        for producto in productos:
+
+            if producto[0] == id_producto:
+
+                nombre = producto[1]
+
+                categoría_producto = producto[2]
+
+                break
+
+
+
+        venta.append(nombre_cliente)  # Agregar el nombre del cliente a la entrada de venta
+
+        venta.append(categoría_producto)  # Agregar la categoría a la entrada de venta
+
+        venta.append(nombre)  # Agregar el nombre del producto a la entrada de venta
+
+
+
+    # Retornar la matriz ventas
+
+    return ventas
